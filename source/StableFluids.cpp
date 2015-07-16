d95ca3ee273ea972218c9c6fadcbd29e35e5d3a0
otherwise777 <commits@onzichtbaar.net>
2015-07-16 11:58:46 +0200

    Revert "updated boundary addings"
    This reverts commit 12742657822a0f45288f0f3afa4a31cfc2c7086d.

--- source/StableFluids.cpp
+++ source/StableFluids.cpp
@@ -17,7 +17,7 @@ static int N;
 static float dt, diff, visc;
 static float force, source;
 static int dvel;
-static bool dboundaries; //boolean to check if boundaries should be added or not.
+static int dboundaries;
 
 static float * u, * v, * u_prev, * v_prev, * squareObject;
 static float * dens, * dens_prev;
@@ -192,17 +192,6 @@ static void draw_boundaries ( void )
 	glEnd ();
 }
 
-static void draw_triangle(void) {
-	//glBegin(GL_TRIANGLES);
-	//glColor3f(0.0f, 1.0f, 0.0f);
-
-	//glVertex2f(0.4, 0.5);
-	//glVertex2f(0.6, 0.6);
-	//glVertex2f(0.6, 0.4);
-
-	//glEnd();
-}
-
 /*
 ----------------------------------------------------------------------
 relates mouse movements to forces sources
@@ -231,12 +220,10 @@ static void get_from_UI ( float * d, float * u, float * v )
 
 	// Draw boundaries
 	if ( mouse_down[0] && dboundaries) {
-		boundary[IX(i, j)] = true;
+		boundary[IX(i,j)] = true;
 	}
-	if (mouse_down[2] && dboundaries) {
-		boundary[IX(i, j)] = false;
-	}
-	if (mouse_down[2] && !dboundaries) {
+
+	if ( mouse_down[2] ) {
 		d[IX(i,j)] = source;
 	}
 
@@ -321,9 +308,6 @@ static void display_func ( void )
 	else		draw_density ();
 
 	draw_boundaries();
-
-	draw_triangle();
-
 
 	post_display ();
 }
